f6e36f9e24b859bafa3f3a6a83ea7b30453ea382
Arnas Jozonis <arnas.jozonis@kilo.health>
2021-03-10 14:28:37 +0200

    add swagger docs generator

--- src/router.js
+++ src/router.js
@@ -5,6 +5,15 @@ import animalController from './resources/animal/animal.controllers'
 const router = Router()
 
 // /api/shelter
+/**
+ * This function comment is parsed by doctrine
+ * @route GET /api
+ * @group foo - Operations about user
+ * @param {string} email.query.required - username or email - eg: user@domain
+ * @param {string} password.query.required - user's password.
+ * @returns {object} 200 - An array of user info
+ * @returns {Error}  default - Unexpected error
+ */
 router
   .route('/')
   .get(shelterController.getAll)
--- src/server.js
+++ src/server.js
@@ -2,8 +2,36 @@ import express from 'express'
 import { json, urlencoded } from 'body-parser'
 import { connect } from './db/db'
 import router from './router'
+import expressSwagger from 'express-swagger-generator'
 
-export const app = express()
+const app = express()
+const swagger = expressSwagger(app)
+
+let options = {
+  swaggerDefinition: {
+    info: {
+      description: 'Animal shelters web service',
+      title: 'Swagger',
+      version: '1.0.0'
+    },
+    host: 'localhost:3000',
+    basePath: '/shelter',
+    produces: ['application/json', 'application/xml'],
+    schemes: ['http', 'https'],
+    securityDefinitions: {
+      JWT: {
+        type: 'apiKey',
+        in: 'header',
+        name: 'Authorization',
+        description: ''
+      }
+    }
+  },
+  basedir: __dirname, // app absolute path
+  files: ['./router.js'] // Path to the API handle folder
+}
+swagger(options)
+
 const port = process.env.PORT || 3000
 app.use(json())
 app.use(urlencoded({ extended: true }))
